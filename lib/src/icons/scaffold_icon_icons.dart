9be905ebb742e06dc71d716f55ccca31f2a20ada
Lowa <clementf7878@gmail.com>
2022-10-17 17:13:11 +0200

    add stif icon part1

--- lib/src/icons/scaffold_icon_icons.dart
+++ lib/src/icons/scaffold_icon_icons.dart
@@ -6,10 +6,11 @@ class Scaffold_icon {
   static const _kFontFam = 'Scaffold_icon';
   static const String? _kFontPkg = null;
 
-  static const IconData cone = IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData clock = IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData metro = IconData(0xe802, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData tram = IconData(0xe803, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData bus = IconData(0xe804, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData train = IconData(0xe805, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData home = IconData(0xe806, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData cone = IconData(0xe807, fontFamily: _kFontFam, fontPackage: _kFontPkg);
 }
